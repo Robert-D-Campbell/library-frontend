527cdff7c82f7917da4230ff1bfc699529c02bcf
Robert Campbell <robert.campbell1191@gmail.com>
2023-10-09 20:30:48 -0700

    chore: update layout for List and Search

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,11 +5,9 @@
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .dashboard {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.dashboard {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
 }
 </style>
